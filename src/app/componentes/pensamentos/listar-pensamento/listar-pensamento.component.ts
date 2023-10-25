dd1c7b06449e9a3a44df71727143bdd25e349d0f
lucasfafa6@gmail.com-at-851479049298 <lucasfafa6@gmail.com-at-851479049298>
2023-10-25 11:07:08 -0300

    Inserindo as diretivas de NgFor e NgIf

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos = [
+    // {
+    //   conteudo: 'Comunicação entre componentes!',
+    //   autoria: 'Angular',
+    //   modelo: 'modelo3'
+    // },
+    // {
+    //   conteudo: 'Imagine um texto muito impactante.',
+    //   autoria: 'Angular',
+    //   modelo: 'modelo2'
+    // }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
